c3f4264b7775d3cfc50b94cf62eb6c80ad22b1fc
ibrahim4529 <ibrahim.hanif4529@gmail.com>
2023-05-26 10:03:23 +0700

    chore: add seeders to make dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\User;
+use App\Models\Task;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,11 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)->create();
+        Task::factory(10)->create();
     }
 }
